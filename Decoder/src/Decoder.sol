99a57bfd0265915b4a5a34ea5a2077f82bbb502a
vo1 <69196155+83demon@users.noreply.github.com>
2026-04-17 17:52:01 +0300

    Completed 1.18 'Application Binary Interface Encoding (abi encoding)'.

--- Decoder/src/Decoder.sol
+++ Decoder/src/Decoder.sol
@@ -10,6 +10,9 @@ contract Decoder {
 
     function decodeData(
         bytes memory _data
-    ) public pure returns (string memory, uint256) {}
+    ) public pure returns (string memory, uint256) {
+		(string memory str, uint256 value) = abi.decode(_data, (string, uint256));
+		return (str, value);
+	}
 }
 
